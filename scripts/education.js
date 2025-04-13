2c35056232b7d4092271558c2a0799e8fc36b341
coporemanuel <jcoporecutipa@byupathway.edu>
2025-04-13 00:42:46 -0400

    third review

--- scripts/education.js
+++ scripts/education.js
@@ -1,16 +1,3 @@
-// const CurrentYear = new Date().getFullYear();
-// document.getElementById('copyright').textContent = `© ${CurrentYear} Inspiring Future Hub, Bermejo-Bolivia`;
-
-// MENU 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const menuBtn = document.getElementById('myButton');
-//     const navigation = document.querySelector('.menu');
-
-//     menuBtn.addEventListener('click', function() {
-//         navigation.classList.toggle('show');
-//         menuBtn.textContent = navigation.classList.contains('show') ? '✖' : '☰';
-//     });
-// });
 
 // EDUCATION 
 async function loadData() {
--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,18 @@ const CurrentYear = new Date().getFullYear();
 document.getElementById('copyright').textContent = `© ${CurrentYear} Inspiring Future Hub, Bermejo-Bolivia`;
 
 // MENU 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.getElementById('myButton');
     const navigation = document.querySelector('.menu');
 
+    // Verificar si el botón y el menú existen en la página actual
     if (menuBtn && navigation) {
         menuBtn.addEventListener('click', function() {
             navigation.classList.toggle('show');
             menuBtn.textContent = navigation.classList.contains('show') ? '✖' : '☰';
         });
+    } else {
+        console.log('No hay menú en esta página.');
     }
 });
 
