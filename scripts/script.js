fc18222399b0e5e787732bbdd6fb9b10adcb4c1b
coporemanuel <jcoporecutipa@byupathway.edu>
2025-04-12 23:03:03 -0400

    firts review

--- scripts/script.js
+++ scripts/script.js
@@ -6,8 +6,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuBtn = document.getElementById('myButton');
     const navigation = document.querySelector('.menu');
 
-    menuBtn.addEventListener('click', function() {
-        navigation.classList.toggle('show');
-        menuBtn.textContent = navigation.classList.contains('show') ? '✖' : '☰';
-    });
+    if (menuBtn && navigation) {
+        menuBtn.addEventListener('click', function() {
+            navigation.classList.toggle('show');
+            menuBtn.textContent = navigation.classList.contains('show') ? '✖' : '☰';
+        });
+    }
 });
+
+
+// CONTACT
+// Get the modal and the button that opens it
+const modal = document.getElementById("contact");
+const openModalButton = document.querySelector("[data-target='support']");
+const closeModalButton = modal.querySelector(".close");
+
+// Open the modal when the button is clicked
+openModalButton.addEventListener("click", () => {
+    modal.style.display = "block";
+});
+
+// Close the modal when the close button is clicked
+closeModalButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close the modal when clicking outside of the modal content
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
